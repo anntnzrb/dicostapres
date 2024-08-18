bd723979aa8dde40537efa2aaf4e9adea2cb69d1
anntnzrb <anntnzrb@proton.me>
2024-08-18 12:39:15 -0500

    flake: bump

--- flake.nix
+++ flake.nix
@@ -1,40 +1,46 @@
 {
   description = "dicostapres";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-    # src tree formatter
-    treefmt-nix.url = "github:numtide/treefmt-nix/main";
-    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
-
-    # poetry + nix
-    poetry2nix.url = "github:nix-community/poetry2nix/master";
-    poetry2nix.inputs.nixpkgs.follows = "nixpkgs";
-
-    # => devenv
-    devenv.url = "github:cachix/devenv/main";
-    devenv.inputs.nixpkgs.follows = "nixpkgs";
-    nix2container.url = "github:nlewo/nix2container/master";
-    nix2container.inputs.nixpkgs.follows = "nixpkgs";
-    mk-shell-bin.url = "github:rrbutani/nix-mk-shell-bin/main";
-  };
-
   nixConfig = {
     extra-trusted-public-keys = [ "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=" ];
     extra-substituters = [ "https://devenv.cachix.org" ];
   };
 
-  outputs = inputs@{ flake-parts, ... }:
+  inputs = {
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    systems.url = "github:nix-systems/default/main";
+
+    # src tree formatter
+    treefmt-nix.url = "github:numtide/treefmt-nix/main";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+
+    # devenv
+    devenv.url = "github:cachix/devenv/main";
+
+    devenv-root.url = "file+file:///dev/null";
+    devenv-root.flake = false;
+
+    nix2container.url = "github:nlewo/nix2container/master";
+    nix2container.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ flake-parts, systems, devenv-root, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = with inputs; [
         devenv.flakeModule
         treefmt-nix.flakeModule
       ];
-      systems = [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      systems = import systems;
 
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
+      perSystem = { config, pkgs, ... }: {
         devenv.shells.default = {
+          devenv.root =
+            let
+              devenvRootFileContent = builtins.readFile devenv-root.outPath;
+              file = devenvRootFileContent;
+            in
+            pkgs.lib.mkIf (file != "") file;
+
           languages = {
             nix.enable = true;
             python = {
